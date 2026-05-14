7e291c858fa6bb9041177ccb0ab0835440edd796
nbarlowATI <nbarlow@turing.ac.uk>
2026-05-14 17:48:01 +0100

    add Infinite scenario, and use LinearPredictor in other scenarios

---
+++ bluebird-gymnasium/tests/envs/test_infinite.py
@@ -0,0 +1,154 @@
+import numpy as np
+import pytest
+import typing
+
+from datetime import timezone
+
+from bluebird_gymnasium.envs import ViewType
+from bluebird_gymnasium.envs.infinite import InfiniteEnv, CustomInfiniteEnv
+from bluebird_gymnasium.utils.types import PositionStatus, ACPositionInfo
+
+EnvCls: typing.TypeAlias = typing.Union[InfiniteEnv, CustomInfiniteEnv]
+
+VIEW_TYPES = list(ViewType)
+ENV_CLASSES = [InfiniteEnv, CustomInfiniteEnv]
+
+def _get_env_instance(view_type: ViewType, env_cls: EnvCls):
+    config = env_cls.get_default_env_config(view_type)
+    gym_env = env_cls(config=config)
+    return gym_env
+
+
+@pytest.mark.parametrize("view_type", VIEW_TYPES)
+@pytest.mark.parametrize("env_cls", ENV_CLASSES)
+def test_init_exceptions(view_type: ViewType, env_cls: EnvCls):
+    """Test initialisation of the gym environment.
+
+    Args:
+        view_type: set to either CENTRALIZED (single agent)
+            or DECENTRALIZED (multi agent) representations.
+        env_cls: defines the gymnasium environment class to use.
+    """
+
+    gym_env = _get_env_instance(view_type, env_cls)
+
+
+@pytest.mark.parametrize("view_type", VIEW_TYPES)
+@pytest.mark.parametrize("env_cls", ENV_CLASSES)
+def test_reset(view_type: ViewType, env_cls: EnvCls):
+    """Test reset of the gym environment.
+
+    Args:
+        view_type: set to either CENTRALIZED (single agent)
+            or DECENTRALIZED (multi agent) representations.
+        env_cls: defines the gymnasium environment class to use.
+    """
+
+    gym_env = _get_env_instance(view_type, env_cls)
+    obs, info = gym_env.reset()
+
+    if view_type == ViewType.CENTRALIZED:
+        assert isinstance(obs, np.ndarray)
+        assert isinstance(info, dict)
+        assert obs.shape == gym_env.observation_space.shape
+    else:  # decentralized
+        assert isinstance(obs, dict)
+        assert isinstance(info, dict)
+        if len(obs) > 0:
+            callsign = list(obs.keys())[0]
+            assert obs[callsign].shape == gym_env.observation_space.shape
+
+
+@pytest.mark.parametrize("view_type", VIEW_TYPES)
+@pytest.mark.parametrize("env_cls", ENV_CLASSES)
+def test_step(view_type: ViewType, env_cls: EnvCls):
+    """Test a forward step (given an action) in the gym environment.
+
+    Args:
+        view_type: set to either CENTRALIZED (single agent)
+            or DECENTRALIZED (multi agent) representations.
+        env_cls: defines the gymnasium environment class to use.
+    """
+
+    gym_env = _get_env_instance(view_type, env_cls)
+    obs, info = gym_env.reset()
+    timestep_before = gym_env.timestep
+
+    if view_type == ViewType.CENTRALIZED:
+        action = 0  # NOOP action.
+        obs, reward, done, truncated, info = gym_env.step(action)
+        timestep_after = gym_env.timestep
+
+        assert isinstance(obs, np.ndarray)
+        assert isinstance(reward, float)
+        assert isinstance(done, bool)
+        assert isinstance(truncated, bool)
+        assert isinstance(info, dict)
+        assert (timestep_before + 1) == timestep_after
+        assert obs.shape == gym_env.observation_space.shape
+
+    else:  # decentralized
+        action = {}  # no action on any aircraft
+        obs, reward, done, truncated, info = gym_env.step(action)
+        timestep_after = gym_env.timestep
+
+        assert isinstance(obs, dict)
+        assert isinstance(reward, dict)
+        assert isinstance(done, dict)
+        assert isinstance(truncated, dict)
+        assert isinstance(info, dict)
+        assert (timestep_before + 1) == timestep_after
+        if len(obs) > 0:
+            callsign = list(obs.keys())[0]
+            assert obs[callsign].shape == gym_env.observation_space.shape
+
+
+@pytest.mark.parametrize("view_type", VIEW_TYPES)
+@pytest.mark.parametrize("env_cls", ENV_CLASSES)
+def test_pos_information(view_type: ViewType, env_cls: EnvCls):
+    """Test `check_pos_information` method of the gym environment.
+
+    Note, after every reset of the environment (and also the underlying
+    simulator), aircraft are always spawned just outside the sector.
+    When a new aircraft is spawned, the position status (pos_status)
+    would be `BEFORE_ENTRY` and `transfer` flag of the aircraft would be
+    `False` as the aircraft is yet to fly through the airspace to reach the
+    exit fix on its filed route.
+
+    Args:
+        view_type: set to either CENTRALIZED (single agent)
+            or DECENTRALIZED (multi agent) representations.
+        env_cls: defines the gymnasium environment class to use.
+    """
+
+    if view_type == ViewType.CENTRALIZED:
+        action = 0
+    else:  # decentralized
+        action = {}  # no action on any aircraft
+
+    gym_env = _get_env_instance(view_type, env_cls)
+    obs, info = gym_env.reset()
+    simulator_env = gym_env.get_simulator_env()
+
+    # check what time the first aircraft is spawned in the
+    # environment and forward the simulation to that time.
+    radar_df = gym_env.get_manager().event_handler.radar_df
+    start_time = radar_df.index.min().replace(tzinfo=timezone.utc).timestamp()
+    start_step = int(start_time // gym_env.scenario_sec_per_step)
+    for _ in range(start_step):
+        gym_env.step(action)
+
+    callsign = list(simulator_env.aircraft.keys())[0]
+    ret: ACPositionInfo = gym_env.check_pos_information(
+        callsign, PositionStatus.BEFORE_ENTRY, False, False, None
+    )
+
+    # in artificial airspace, we can expect that the first aircraft
+    # at start of the scenario is yet to enter the airspace/sector.
+    assert ret.position_status == PositionStatus.BEFORE_ENTRY
+    assert ret.incomm_status is False
+    assert ret.outcomm_status is False
+    assert ret.dist_to_sector_entry > 0.0
+    assert ret.dist_away_from_sector_exit == 0.0
+    assert ret.dist_away_from_incorrect_sector_exit == 0.0
+    assert ret.incorrect_exit_position is None
